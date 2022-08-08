984fc24b624ae6f8f02737f174d240ec89c0a413
DouglasVulcano <vulcanodouglas1708@gmail.com>
2022-08-07 23:32:32 -0300

    typescript with vue3 first commit

---
+++ src/views/IndexPage.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="home">
+    <h1>Hello World!</h1>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'IndexPage',
+  components: {
+  },
+});
+</script>
