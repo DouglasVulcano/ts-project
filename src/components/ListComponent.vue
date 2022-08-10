3b1d40c17aaa9846847335c76212f476264acb8e
DouglasVulcano <vulcanodouglas1708@gmail.com>
2022-08-09 21:07:02 -0300

    finish example project

--- src/components/ListComponent.vue
+++ src/components/ListComponent.vue
@@ -1,13 +1,18 @@
 <template>
   <div class="job-list">
     <ul>
-      <li v-for="job in jobs" :key="job.id">
+      <li v-for="job in orderedJobs" :key="job.id">
         <h2>{{ job.title }} in {{ job.location }}</h2>
         <div class="salary">
           <p>{{ job.salary }}</p>
         </div>
         <div class="description">
-          It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+          It has survived not only five centuries, but also the leap into
+          electronic typesetting, remaining essentially unchanged. It was
+          popularised in the 1960s with the release of Letraset sheets
+          containing Lorem Ipsum passages, and more recently with desktop
+          publishing software like Aldus PageMaker including versions of Lorem
+          Ipsum.
         </div>
       </li>
     </ul>
@@ -15,7 +20,7 @@
 </template>
 
 <script lang="ts">
-import { defineComponent, PropType } from "vue";
+import { computed, defineComponent, PropType } from "vue";
 import Job from "src/types/Job";
 import OrderTerm from "src/types/OrderTerm";
 
@@ -33,42 +38,46 @@ export default defineComponent({
     },
   },
 
-  setup() {
-    return {};
+  setup(props) {
+    const orderedJobs = computed(() => {
+      return [...props.jobs].sort((a: Job, b: Job) => {
+        return a[props.order] > b[props.order] ? 1 : -1;
+      });
+    });
+
+    return { orderedJobs };
   },
 });
 </script>
 
-
 <style scoped>
-  .job-list {
-    max-width: 860px;
-    margin: 40px auto;
-  }
-  .job-list ul {
-    padding: 0;
-  }
-  .job-list li {
-    list-style-type: none;
-    background: white;
-    padding: 16px;
-    margin: 16px 0;
-    border-radius: 4px;
-  }
-  .job-list h2 {
-    margin: 0 0 10px;
-    text-transform: capitalize;
-  }
-  .salary {
-    display: flex;
-  }
-  .salary img {
-    width: 30px;
-  }
-  .salary p {
-    color: #17bf66;
-    font-weight: bold;
-    margin: 10px 4px;
-  }
+.job-list {
+  max-width: 860px;
+  margin: 40px auto;
+}
+.job-list ul {
+  padding: 0;
+}
+.job-list li {
+  list-style-type: none;
+  background: white;
+  padding: 16px;
+  margin: 16px 0;
+  border-radius: 4px;
+}
+.job-list h2 {
+  margin: 0 0 10px;
+  text-transform: capitalize;
+}
+.salary {
+  display: flex;
+}
+.salary img {
+  width: 30px;
+}
+.salary p {
+  color: #17bf66;
+  font-weight: bold;
+  margin: 10px 4px;
+}
 </style>
-
--- src/views/IndexPage.vue
+++ src/views/IndexPage.vue
@@ -55,7 +55,7 @@ export default defineComponent({
 });
 </script>
 
-<style scoped>
+<style scoped>  
 header {
   text-align: center;
 }
